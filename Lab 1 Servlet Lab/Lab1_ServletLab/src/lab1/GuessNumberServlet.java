7735e4199ba1cfd6c4663533833ae94aaceaced1
Julia Chen <juliachenjing@gmail.com>
2018-02-25 14:37:46 -0500

    updated

--- Lab 1 Servlet Lab/Lab1_ServletLab/src/lab1/GuessNumberServlet.java
+++ Lab 1 Servlet Lab/Lab1_ServletLab/src/lab1/GuessNumberServlet.java
@@ -10,29 +10,34 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/GuessNumber")
 public class GuessNumberServlet extends HttpServlet {
-    /**
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
 	@Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        PrintWriter out = response.getWriter();
-        out.print("<html><head><title>Guess A Number</title></head><body>");
-        out.print("<form method='post'>");
-        out.print("<p>Please Choose a number</p>");
-        out.print("<input type='submit' value='Try this number'/>");
-        out.print("</form>");
-        out.print("</body></html>");
-    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.print("<html><head><title>Guess A Number</title></head><body>");
+		out.println("<form method='get'>");
+		out.print("<p>Please Choose a number</p>");
+		out.println("number=<input type=text name=number>");
+		out.println("<input type=submit value='Try this number'>");
+		out.println("</form>");
+		out.print("</body></html>");
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        PrintWriter out = response.getWriter();
-        out.print("<html><head><title>Test</title></head><body>");
-        out.print("<p>from doPost method: Postback received</p>");
-        out.print("</body></html>");
-    }
+		final String targetNumber = "8";
+		if (request.getParameter("number").equals(targetNumber)) {
+			out.print("<p>You Got it </p>");
+		} else
+			out.print("<p>Wrong Number </p>");
+		out.close();
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
 }
--- Lab 1 Servlet Lab/Lab1_ServletLab/src/lab1/SimplestGetPostServlet.java
+++ Lab 1 Servlet Lab/Lab1_ServletLab/src/lab1/SimplestGetPostServlet.java
@@ -10,29 +10,29 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/Simplest")
 public class SimplestGetPostServlet extends HttpServlet {
-    /**
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
 	@Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        PrintWriter out = response.getWriter();
-        out.print("<html><head><title>Test</title></head><body>");
-        out.print("<form method='post'>");
-        out.print("<p>from doGet method : Please click the button</p>");
-        out.print("<input type='submit' value='Click me'/>");
-        out.print("</form>");
-        out.print("</body></html>");
-    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.print("<html><head><title>Test</title></head><body>");
+		out.print("<form method='post'>");
+		out.print("<p>from doGet method : Please click the button</p>");
+		out.print("<input type='submit' value='Click me'/>");
+		out.print("</form>");
+		out.print("</body></html>");
+	}
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        PrintWriter out = response.getWriter();
-        out.print("<html><head><title>Test</title></head><body>");
-        out.print("<p>from doPost method: Postback received</p>");
-        out.print("</body></html>");
-    }
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.print("<html><head><title>Test</title></head><body>");
+		out.print("<p>from doPost method: Postback received</p>");
+		out.print("</body></html>");
+	}
 }
