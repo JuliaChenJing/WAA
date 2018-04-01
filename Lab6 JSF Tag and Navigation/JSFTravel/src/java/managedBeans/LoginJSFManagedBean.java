115b2ca38c4a4510e7fe884c9b418b9cf6f0f4a0
Julia Chen <juliachenjing@gmail.com>
2018-04-01 14:33:15 -0400

    added first class

--- Lab6 JSF Tag and Navigation/JSFTravel/src/java/managedBeans/LoginJSFManagedBean.java
+++ Lab6 JSF Tag and Navigation/JSFTravel/src/java/managedBeans/LoginJSFManagedBean.java
@@ -32,7 +32,7 @@ public class LoginJSFManagedBean {
     }
 
     public String submit() {
-        if (this.userId.equals("julia") && this.password.equals("yoyo")) {
+        if (this.userId.equals("admin") && this.password.equals("admin")) {
             return "loginSuccess.xhtml";
         } else {
             return "loginFailure.xhtml";
--- Lab6 JSF Tag and Navigation/JSFTravel/src/java/managedBeans/TravelInformationJSFManagedBean.java
+++ Lab6 JSF Tag and Navigation/JSFTravel/src/java/managedBeans/TravelInformationJSFManagedBean.java
@@ -17,6 +17,48 @@ public class TravelInformationJSFManagedBean {
     String departureDate;
     String returnDate;
 
+    boolean firstClass;
+    boolean exactTheseDates;
+    boolean noStopover;
+    String options = "";
+
+    public String getOptions() {
+        return options;
+    }
+
+    public void setOptions(String options) {
+        this.options = options;
+    }
+
+    public void setInactive(boolean inactive) {
+        this.inactive = inactive;
+    }
+    boolean inactive;
+
+    public boolean isFirstClass() {
+        return firstClass;
+    }
+
+    public void setFirstClass(boolean firstClass) {
+        this.firstClass = firstClass;
+    }
+
+    public boolean isExactTheseDates() {
+        return exactTheseDates;
+    }
+
+    public void setExactTheseDates(boolean exactTheseDates) {
+        this.exactTheseDates = exactTheseDates;
+    }
+
+    public boolean isNoStopover() {
+        return noStopover;
+    }
+
+    public void setNoStopover(boolean noStopover) {
+        this.noStopover = noStopover;
+    }
+
     public String getFrom() {
         return from;
     }
@@ -50,6 +92,22 @@ public class TravelInformationJSFManagedBean {
     }
 
     public String submit() {
+        if (this.firstClass) {
+            this.options = this.options + "first Class";
+        }
+
+        if (this.exactTheseDates) {
+            if (!this.options.equals("")) {
+                this.options = this.options + ",";
+            }
+            this.options = this.options + "exact these dates";
+        }
+        if (this.noStopover) {
+            if (!this.options.equals("")) {
+                this.options = this.options + ",";
+            }
+            this.options = this.options + "no stopover";
+        }
         return "travelInformationSubmitSuccess.xhtml";
     }
 }
