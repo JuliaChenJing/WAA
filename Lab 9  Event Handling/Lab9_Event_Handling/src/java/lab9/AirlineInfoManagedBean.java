721757005851f51341048327ab0049d0f09c34e4
Julia Chen <juliachenjing@gmail.com>
2018-04-28 22:53:08 -0400

    everyting is there but not working

--- Lab 9  Event Handling/Lab9_Event_Handling/src/java/lab9/AirlineInfoManagedBean.java
+++ Lab 9  Event Handling/Lab9_Event_Handling/src/java/lab9/AirlineInfoManagedBean.java
@@ -9,12 +9,15 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.event.ValueChangeEvent;
 
 @ManagedBean
 @RequestScoped
 public class AirlineInfoManagedBean {
 
     String selectedAirline;
+    private List<AirlineInfo> airlineInfoList;
+    private List<AirlineInfo> selectedAirlineInfoList;
 
     public String getSelectedAirline() {
         return selectedAirline;
@@ -24,9 +27,8 @@ public class AirlineInfoManagedBean {
         this.selectedAirline = selectedAirline;
     }
 
-    private List<AirlineInfo> airlineInfoList = new ArrayList<AirlineInfo>();
-
     public AirlineInfoManagedBean() {
+        airlineInfoList = new ArrayList<AirlineInfo>();
         airlineInfoList.add(new AirlineInfo("Amstardam", "New York", "12/11/06", "KLM", 790.80));
         airlineInfoList.add(new AirlineInfo("Amstardam", "New York", "12/11/06", "Morth West", 795.55));
         airlineInfoList.add(new AirlineInfo("Amstardam", "New York", "12/11/06", "KLM", 820.00));
@@ -56,5 +58,24 @@ public class AirlineInfoManagedBean {
 
     public void reset() {
         selectedAirline = "All";
+    }
+
+    public void selectedAirlineChanged(ValueChangeEvent e) {
+        if (e.getNewValue().toString().equals("All")) {
+
+            this.selectedAirlineInfoList = airlineInfoList;
+        } else {
+            this.selectedAirlineInfoList = getSelectedAirlineInfoList(e.getNewValue().toString());
+        }
+    }
+
+    public List<AirlineInfo> getSelectedAirlineInfoList(String selectedAirline) {
+        this.selectedAirlineInfoList = new ArrayList<AirlineInfo>();
+        for (AirlineInfo item : airlineInfoList) {
+            if (item.airline.equals(selectedAirline)) {
+                this.selectedAirlineInfoList.add(item);
+            }
+        }
+        return airlineInfoList;
     }
 }
