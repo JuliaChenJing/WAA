da04fd3ba93f577b3e75b5679a8a920c237ad6d9
Julia Chen <juliachenjing@gmail.com>
2018-05-20 19:22:09 -0400

    shopping cart rename finished

--- Lab 12 Shopping Cart/Lab12_Shopping_Cart/src/java/lab7/ShoppingCartManagedBean.java
+++ Lab 12 Shopping Cart/Lab12_Shopping_Cart/src/java/lab7/ShoppingCartManagedBean.java
@@ -5,8 +5,9 @@
  */
 package lab7;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.ArrayList;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
@@ -14,27 +15,31 @@ import javax.faces.bean.RequestScoped;
 @RequestScoped
 public class ShoppingCartManagedBean {
 
-    private List<ShoppingCartItem> employeelist = new ArrayList<>();
-    private ShoppingCartItem selectedEmployee;
+    private List<ShoppingCartItem> shoppingCartList = new ArrayList<ShoppingCartItem>();
+
+    public List<ShoppingCartItem> getShoppingCartList() {
+        return shoppingCartList;
+    }
+
+    public void setShoppingCartList(List<ShoppingCartItem> shoppingCartList) {
+        this.shoppingCartList = shoppingCartList;
+    }
+
+    public HashMap<Integer, Integer> getQuantityMap() {
+        return quantityMap;
+    }
+
+    public void setQuantityMap(HashMap<Integer, Integer> quantityMap) {
+        this.quantityMap = quantityMap;
+    }
+
+    private HashMap<Integer, Integer> quantityMap
+            = new HashMap<Integer, Integer>();
 
     public ShoppingCartManagedBean() {
-        employeelist.add(new ShoppingCartItem(1432, "The winnner takes it all", 12.95));
-        employeelist.add(new ShoppingCartItem(1321, "Yellow submarine", 11.35));
-    }
-
-    public void setSelectedEmployee(ShoppingCartItem SelectedEmployee) {
-        this.selectedEmployee = SelectedEmployee;
-    }
-
-    public ShoppingCartItem getSelectedEmployee() {
-        return selectedEmployee;
-    }
-
-    public List<ShoppingCartItem> getEmployeelist() {
-        return employeelist;
-    }
-
-    public void setEmployeelist(List<ShoppingCartItem> employeelist) {
-        this.employeelist = employeelist;
+        shoppingCartList.add(new ShoppingCartItem(1432, "The winnner takes it all", 12.95));
+        shoppingCartList.add(new ShoppingCartItem(1321, "Yellow submarine", 11.35));
+        quantityMap.put(1432, 1);
+        quantityMap.put(1321, 2);
     }
 }
